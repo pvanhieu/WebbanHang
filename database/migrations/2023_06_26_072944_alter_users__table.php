739994359e547797ab31d4f7625c7643d0b5f201
cunyear <phamvanhieu2108@gmail.com>
2023-06-26 23:42:23 +0700

    create database

---
+++ database/migrations/2023_06_26_072944_alter_users__table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+        $table->string('phone_number',20);
+        $table->string('address',200);
+        $table->tinyInteger('deleted')->default(0);
+        $table->unsignedBigInteger('role_id');
+        $table->foreign('role_id')->references('id')->on('role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
